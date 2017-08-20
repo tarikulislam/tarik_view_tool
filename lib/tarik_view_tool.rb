a600c2b6f7cb397cd4c098c95a818cf7fddedfc9
tarikulislam <tarikulislam.tar@gmail.com>
2017-08-20 10:41:39 +0600

    Dev tool initial commit

---
+++ lib/tarik_view_tool.rb
@@ -0,0 +1,6 @@
+require "tarik_view_tool/version"
+require "tarik_view_tool/renderer"
+
+module TarikViewTool
+  # Your code goes here...
+end
---
+++ lib/tarik_view_tool/version.rb
@@ -0,0 +1,3 @@
+module TarikViewTool
+  VERSION = "0.1.0"
+end
